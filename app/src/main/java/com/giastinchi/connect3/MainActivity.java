6b01e188dbe29f13e78d4e82156be624842ab1ca
jimmikid <gianmarco.stinchi@hotmail.com>
2017-09-14 22:26:27 +0200

    Finished App For 1v1 Connect 3
    So far i decided to  stop the app development. Now it's possible to play
    1v1, but not against the Computer. This functionality will be
    implemented later.
    1 - Setted Up the winning conditions and the winner result
    2 - Setted Up the draw conditions
    3 - Setted Up the Play Again function

--- app/src/main/java/com/giastinchi/connect3/MainActivity.java
+++ app/src/main/java/com/giastinchi/connect3/MainActivity.java
@@ -3,7 +3,10 @@ package com.giastinchi.connect3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +14,13 @@ public class MainActivity extends AppCompatActivity {
     //The second player = yellow = 1
     int activePlayer = 0;
 
+    boolean isActive = true;
+
     //Value 2 means that the slot is free
     int[] gameState = {2,2,2,2,2,2,2,2,2};
+
+    //Those are the triples of rows, colums, diagonals who allows the player to win
+    int [][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
 
     public void dropIn(View view){
 
@@ -20,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         int counterTag = Integer.parseInt(counter.getTag().toString());
 
-        if(gameState[counterTag] == 2){
+        if(gameState[counterTag] == 2 && isActive){
 
             counter.setTranslationY(-1000f);
 
@@ -41,6 +49,78 @@ public class MainActivity extends AppCompatActivity {
                 activePlayer = 0;
 
             }
+
+            for(int[]winningPosition : winningPositions){
+
+                if(gameState[winningPosition[0]] == gameState[winningPosition[1]]
+                        && gameState[winningPosition[1]] == gameState[winningPosition[2]]
+                        && gameState[winningPosition[0]] != 2){
+
+                    isActive = false;
+
+                    String winner = "";
+                    //Blue has won
+                    if(gameState[winningPosition[0]] == 0){
+
+                        winner = "Blue";
+
+                    }else{
+
+                        winner = "Yellow";
+
+                    }
+
+                    TextView winnerMessage = (TextView)findViewById(R.id.winnerMessage);
+                    winnerMessage.setText(winner + " has won!");
+
+                    //The winner has been chosen so displays play again Message
+                    LinearLayout linearLayout = (LinearLayout)findViewById(R.id.playAgainLayout);
+                    linearLayout.setVisibility(View.VISIBLE);
+
+                }else{
+
+                    boolean gameIsOver = true;
+
+                    for(int counterState : gameState) {
+
+                        if (counterState == 2) gameIsOver = false;
+
+                    }
+                    if(gameIsOver){
+
+                            TextView winnerMessage = (TextView)findViewById(R.id.winnerMessage);
+                            winnerMessage.setText("It's a Draw");
+                            LinearLayout linearLayout = (LinearLayout)findViewById(R.id.playAgainLayout);
+                            linearLayout.setVisibility(View.VISIBLE);
+
+                    }
+
+
+                }
+            }
+        }
+    }
+
+    public void playAgain(View view){
+
+        activePlayer = 0;
+
+        isActive = true;
+
+        for (int i = 0; i < gameState.length; i++){
+
+            gameState[i] = 2;
+
+        }
+
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.playAgainLayout);
+        linearLayout.setVisibility(View.INVISIBLE);
+
+        GridLayout gridLayout = (GridLayout)findViewById(R.id.gridLayout);
+        for (int i = 0; i < gridLayout.getChildCount(); i++){
+
+            ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
+
         }
 
     }
